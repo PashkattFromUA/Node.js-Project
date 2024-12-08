e12bbc9ff350b997a3c207b36a24f3bf9345d7e2
PashkattFromUA <pashkattfromua@gmail.com>
2024-12-08 13:58:30 +0100

    added prettier

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-const express = require("express");
+const express = require('express');
 
 const app = express();
 const port = 3000;
@@ -6,5 +6,5 @@ const port = 3000;
 app.use(express.json());
 
 app.listen(port, () => {
-  console.log(`Server running at http://localhost:${port}`);
+    console.log(`Server running at http://localhost:${port}`);
 });
