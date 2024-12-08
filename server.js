f76c7003a6b29c478f30369f772d1a70ff417cb4
PashkattFromUA <pashkattfromua@gmail.com>
2024-12-08 20:00:35 +0100

    Created getTasks endpoints

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ import express from 'express';
 import './config/db.js';
 
 import authRouter from './routes/authRoutes.js';
+import taskRouter from './routes/taskRoutes.js'
 
 const app = express();
 const port = 3000;
@@ -9,6 +10,7 @@ const port = 3000;
 app.use(express.json());
 
 app.use('/api', authRouter);
+app.use('/api', taskRouter);
 
 app.listen(port, () => {
     console.log(`Server running at http://localhost:${port}`);
